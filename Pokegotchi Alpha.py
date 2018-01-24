15f0d0ee959d6488ada8f2e59d3c335c55a8dba1
Patrick Fox <Patrick.fox1307@outlook.com>
2018-01-24 14:12:57 +0000

    PokeGotchi Alpha V1.2 (Stable)
    STABLE EDITION
    Updates - 
    1. Main Menu Added
    2. Bug Fixes

--- Pokegotchi Alpha.py
+++ Pokegotchi Alpha.py
@@ -1,8 +1,25 @@
+print ("Welcome to PokeGotchi! / (C) Patrick 2018 / You may find source code for this on https://github.com/GalaxyTheoryz/pokegotchi.")
+print ("This edition comes with two languages, English and Japanese.")
+print ("ENJOY!")
+print ("PokeGotchiへようこそ！ /（C）Patrick 2018 /これのソースコードはhttps://github.com/GalaxyTheoryz/pokegotchiにあります。")
+print ("このエディションには、英語と日本語の2つの言語があります。")
+print ("楽しい！")
+print ("********************************************************************************")
+print ("********************************************************************************")
+lic= input ("BY USING THIS PROGRAM, YOU ACCEPT THE LICENSE INCLUDED WITH THIS PROGRAM. A COPY CAN BE FOUND HERE: https://github.com/GalaxyTheoryz/pokegotchi/blob/master/LICENSE Y/N")
+
+print ("********************************************************************************")
+print ("********************************************************************************")
+
+
+
 import sys
 print ("Hello and welcome to Pokegotchi. こんにちは、Pokegotchiへようこそ。")
 name= input ("What would you like to call your Pokegotchi? あなたはあなたのポケッと呼びたいと思いますか？")
-print ("Hello " + name)
+gender= input ("What Gender is your Pokegotchi? Male / Female / Other? あなたのポケッとは何ですか？男性/女性/その他？")
+print ("Hello User, I like your name, which is " + name)
 print ("こんにちは " + name)
+print ("Great Choice on your Gender, which is " + gender)
 
 start= input ("Would you like to begin game Y/N? あなたはゲームを始めたいですか Y / N？")
 if start== "N":
@@ -67,11 +84,6 @@ while boom >0:
     print(boom)
     boom -=1
     
-happiness = 100
-hunger = 100
-thirst = 100
-health = 100
-
 while happiness >0:
  happiness -=1
  print (happiness)
@@ -113,3 +125,111 @@ while boom >0:
     time.sleep(1)
     print(boom)
     boom -=1
+print ("Your Pokegotchi has slept well. あなたのPokegotchiはよく眠っています。")
+
+if sleep=="N":
+    print ("Your PokeGotchi does require to sleep, if not they may die!")
+
+    print ("HAPPINESS = 90, SLEEP = 20, THIRST = 50, HUNGER = 100")
+    # Note to self: Put in variable at future stage.
+
+drink= input ("Your Pokegotchi requires a drink! Y/N? あなたのPokegotchiは飲み物が必要です！ Y / N？")
+if drink=="Y":
+    boom=10
+while boom >0:
+    time.sleep(1)
+    print(boom)
+    boom -=1
+    print ("Your PokeGotchi drank well! あなたのPokeGotchiはうまく飲んだ！")
+
+menu= input ("Welcome to your Main Menu, please select an option. 1 - Re-cycle through all options. 2 - Play. 3 - Exit. Please select a number.")
+if menu=="1":
+    feed = input ("Would you like to feed your Pokegotchi? Y/N  あなたはPokegotchiを食べたいですか？ Y / N")
+if feed== "Y":
+    print ("Feeding is in Progress")
+import time
+print ("Feeding your Pokegotchi!  あなたのPokegotchiを与える！")
+boom=20
+while boom >0:
+    time.sleep(1)
+    print(boom)
+    boom -=1
+    
+while happiness >0:
+ happiness -=1
+ print (happiness)
+
+while hunger >0:
+ hunger -=1
+ print (hunger)
+
+while thirst >0:
+ thirst -=1
+ print (thirst)
+
+while health >0:
+ health -=1
+ print (health)
+
+print ("\:.             .:/")
+print ("        \``._________.''/ ")
+print ("         \             / ")
+print (" .--.--, / ..:.   ..:. \\")
+print ("/__:  /  | '::' . '::' |")
+print ("   / /   |`.   ._.   .'|")
+print ("  / /    |.'         '.|")
+print (" /___-_-,|.\  \   /  /.|")
+print ("      // |''\.;   ;,/ '|")
+print ("      `==|:=         =:|")
+print ("         `.          .'")
+print ("           :-._____.-:")
+print ("          `''       `''")
+
+sleep = input ("Would you like to make your Pokegotchi sleep? Pokegotchiを眠らせたいですか？")
+
+if sleep== "Y":
+    print ("Sleeping is in Progress! Do not Disturb!")
+    import time
+print ("Your Pokegotchi is Sleeping! あなたのPokegotchiは眠っている！")
+boom=40
+while boom >0:
+    time.sleep(1)
+    print(boom)
+    boom -=1
+print ("Your Pokegotchi has slept well. あなたのPokegotchiはよく眠っています。")
+
+if sleep=="N":
+    print ("Your PokeGotchi does require to sleep, if not they may die!")
+
+    print ("HAPPINESS = 90, SLEEP = 20, THIRST = 50, HUNGER = 100")
+    # Note to self: Put in variable at future stage.
+
+drink= input ("Your Pokegotchi requires a drink! Y/N? あなたのPokegotchiは飲み物が必要です！ Y / N？")
+if drink=="Y":
+    boom=10
+while boom >0:
+    time.sleep(1)
+    print(boom)
+    boom -=1
+    print ("Your PokeGotchi drank well! あなたのPokeGotchiはうまく飲んだ！")
+
+if menu=="2":
+ boom=30
+ while boom >0:
+    time.sleep(1)
+    print(boom)
+    boom -=1
+
+if menu=="3":
+    sys.exit()
+    
+    
+    
+
+    
+    
+
+
+
+
+    
